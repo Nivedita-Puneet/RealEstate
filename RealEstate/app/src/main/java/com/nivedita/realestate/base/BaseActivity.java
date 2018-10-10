b820246a01ba422d73a34d76817fba6ccba2f16e
Nivedita-puneet <nivedita.puneet@gmail.com>
2018-10-10 14:34:58 +1100

    Defined Base Activity and Property List Adapter

---
+++ RealEstate/app/src/main/java/com/nivedita/realestate/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.nivedita.realestate.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A base activity which is extended by Property List Activity, It handles all the basic operations.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+
+
+}
---
+++ RealEstate/app/src/main/java/com/nivedita/realestate/propertylist/PropertyListAdapter.java
@@ -0,0 +1,11 @@
+package com.nivedita.realestate.propertylist;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * PropertyList Adapter which we will bind to the recycler view.
+ */
+
+public class PropertyListAdapter{
+    
+}
